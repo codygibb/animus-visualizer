a963962019e8612f56747a5e383acd6caff318b1
codygibb <codyhgibb@gmail.com>
2014-04-10 16:30:43 -0700

    added status messages for tmp dir

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #cd into the directory that Animus is in, then run this script to launch the visualizer
 
+echo "creating temporary bin"
 CWD=$(pwd)
 processing-java --sketch=$CWD --output="$CWD/tmp" --force --present
 
-# --output creates a folder for all class files, so this deletes it afterwards
+echo "removing temporary bin"
 rm -rf tmp
 
